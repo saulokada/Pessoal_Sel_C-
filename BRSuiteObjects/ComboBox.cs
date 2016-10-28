4351092a29bd1d77a1c51a8bab79544922ba4fb8
saulokada <saulokada@gmail.com>
2016-10-28 11:56:25 -0200

    Add files via upload

---
+++ BRSuiteObjects/ComboBox.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace Selenium.Utils.BRSuiteObjects
+{
+    public class ComboBox
+    {
+        //Fará a ponte com o navegador que será utilizado nos testes
+        private IWebDriver _driver;
+
+        public ComboBox(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void selecionarItemComboBox(string nomeComboBox, int valor)
+        {
+            IWebElement cboComboBox = _driver.FindElement(By.Id(nomeComboBox));
+            SelectElement selector = new SelectElement(cboComboBox);
+            selector.SelectByIndex(valor);    
+        }
+    }
+}
